1a21931fa6b2f1f80d02b34c675206cee3889d56
xTheCoderx <justaguyoney@gmail.com>
2021-10-19 01:29:38 +1300

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/CMemoryAlloc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMemoryAlloc.dir/main.c.obj"
+  "CMemoryAlloc.exe"
+  "CMemoryAlloc.exe.manifest"
+  "CMemoryAlloc.pdb"
+  "libCMemoryAlloc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMemoryAlloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
